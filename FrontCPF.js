e237aa144d040436070ea5dc4e72da1ac380f04c
Joana Luiza <joanasilva@uniaraxa.edu.br>
2023-11-27 18:36:26 -0300

    atulizado 2

--- FrontCPF.js
+++ FrontCPF.js
@@ -1,24 +1,34 @@
-function BuscarApi(){
-
-  var Recebido = document.getElementById('caixa-texto').value;
-  fetch('https://localhost:7078/api/Verificador?CPF=' + Recebido) 
-    .then(response => {
+function BuscarAPI() {
+  var cpfInput = document.getElementById('caixa-texto').value;
+  fetch('https://localhost:7078/api/Verificador?CPF=' + cpfInput)
+    .then((response) => {
       return response.json();
-      console.log(response);
     })
-    .then(data => {
-      /*
-      data.forEach(item => {
-          console.log('Nome:', item.nome);
-          console.log('Idade:', item.idade);
-          console.log('Email:', item.email);
-          console.log('---');
-      */
-        });
-      // Faça o que precisar com os dados aqui
-    })
-    .catch(error => {
-      // Caso ocorra algum erro na requisição
-      console.error('Ocorreu um erro:', error);
+    .then((responseData) => {
+      if (responseData['valido'] == false) {
+        cpfOK.style.display = "none";
+        MensagemSucesso.style.display = "none";
+        mensagemErro.style.display = "block";
+        respostaErro.innerHTML =  data['Mensagem'];
+      }
+      else {
+        cpfOK.style.display = "block";
+        MensagemSucesso.style.display = "block";
+        mensagemErro.style.display = "none";
+      }
+      CPFFormatado.innerHTML = responseData['cpf'];
+      EstadoCPF.innerHTML = responseData['Estado'];
+
+      DataHora.innerHTML = responseData['Data'];
     });
 }
+
+
+function Inicio() {
+  cpfOK.style.display = "none";
+  MensagemSucesso.style.display = "none";
+  mensagemErro.style.display = "none";
+}
+
+window.onload = Inicio;
+
